c28b1136e38638d67a8a1ce621c72cfe14f329f7
Ahmed Tarek <a.tarek360@gmail.com>
2018-11-18 16:18:53 +0700

    fix file path by using filePath instead of class path

--- test-coverage-rule/src/test/kotlin/io/github/tarek360/rules/coverage/CsvParserImplTest.kt
+++ test-coverage-rule/src/test/kotlin/io/github/tarek360/rules/coverage/CsvParserImplTest.kt
@@ -22,23 +22,23 @@ class CsvParserImplTest {
         result mustHaveSize 12
 
         result[0].classPackage mustEqual "io.github.tarek360.magic"
-        result[0].fileName mustEqual "Lines"
-        result[0].filePath mustEqual "io/github/tarek360/magic/Lines"
+        result[0].className mustEqual "Lines"
+        result[0].classPath mustEqual "io/github/tarek360/magic/Lines"
         result[0].coveredBranches mustEqual 50
 
         result[1].classPackage mustEqual "io.github.tarek360.gitdiff"
-        result[1].fileName mustEqual "GitFileImpl"
-        result[1].filePath mustEqual "io/github/tarek360/gitdiff/GitFileImpl"
+        result[1].className mustEqual "GitFileImpl"
+        result[1].classPath mustEqual "io/github/tarek360/gitdiff/GitFileImpl"
         result[1].coveredBranches mustEqual 80
 
         result[2].classPackage mustEqual "io.github.tarek360.gitdiff"
-        result[2].fileName mustEqual "Parser.StartCount"
-        result[2].filePath mustEqual "io/github/tarek360/gitdiff/Parser\$StartCount"
+        result[2].className mustEqual "Parser.StartCount"
+        result[2].classPath mustEqual "io/github/tarek360/gitdiff/Parser\$StartCount"
         result[2].coveredBranches mustEqual 100
 
         result[3].classPackage mustEqual "io.github.tarek360.gitdiff"
-        result[3].fileName mustEqual "Controller"
-        result[3].filePath mustEqual "io/github/tarek360/gitdiff/Controller"
+        result[3].className mustEqual "Controller"
+        result[3].classPath mustEqual "io/github/tarek360/gitdiff/Controller"
         result[3].coveredBranches mustEqual 0
 
 
