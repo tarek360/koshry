5a22805a70345fab1e6fb52c710e9909fdb8ce8a
Ahmed Tarek <a.tarek360@gmail.com>
2019-08-04 17:31:33 +0700

    More logs (#26)
    * add more logs to GitHostInfoProvider
    
    * VERSION=0.0.4.5

--- koshry/src/main/java/io/github/tarek360/koshry/git/GitRemoteUrlParser.kt
+++ koshry/src/main/java/io/github/tarek360/koshry/git/GitRemoteUrlParser.kt
@@ -1,16 +1,22 @@
 package io.github.tarek360.koshry.git
 
+import io.github.tarek360.core.logger
+
 open class GitRemoteUrlParser {
 
     open fun parse(remoteOriginUrl: String): GitRemoteOrigin? {
 
-        val url = remoteOriginUrl.split(':')
+        try {
+            val url = remoteOriginUrl.split(':')
 
-        if (url.size == 2) {
-            val domain = url[0].removePrefix("git@")
-            val ownerNameRepoName = url[1].removeSuffix(".git")
-            return GitRemoteOrigin(domain, ownerNameRepoName)
+            if (url.size == 2) {
+                val domain = url[0].removePrefix("git@")
+                val ownerNameRepoName = url[1].removeSuffix(".git")
+                return GitRemoteOrigin(domain, ownerNameRepoName)
+            }
 
+        } catch (e: Exception) {
+            logger.e { "Can't parse: $remoteOriginUrl" }
         }
         return null
     }
