c8b615c876570109dd222f599563a97cef8f4379
Ahmed Tarek <a.tarek360@gmail.com>
2019-08-05 12:30:21 +0700

    Release 0.0.4.6 (#27)
    * add more logs to Environment.getVariable
    
    * VERSION=0.0.4.6

--- ci-detector/src/main/java/io/github/tarek360/ci/Environment.kt
+++ ci-detector/src/main/java/io/github/tarek360/ci/Environment.kt
@@ -2,6 +2,22 @@ package io.github.tarek360.ci
 
 class Environment {
     companion object {
-        fun getVariable(name: String): String? = System.getenv(name)
+        fun getVariable(name: String): String? {
+            return try {
+                val value = System.getenv(name)
+                if (value == null)
+                    printError("Value for [$name] Environment Variable is null!")
+                value
+            } catch (e: Exception) {
+                printError("Can't getVariable for [$name], Exception: ${e.javaClass.name}: ${e.message}")
+                e.printStackTrace()
+                null
+            }
+        }
+
+        private fun printError(msg: String) {
+            val ansiRed = "\u001B[31m"
+            println("$ansiRed$msg$ansiRed")
+        }
     }
 }
